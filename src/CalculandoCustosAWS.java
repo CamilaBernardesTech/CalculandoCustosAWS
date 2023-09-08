95b35f2ee6d5f2dfe030353868dc6daf8141126b
Camila Bernardes <camilaflsbernardes@icloud.com>
2023-09-08 16:15:18 -0300

    first commit

---
+++ src/CalculandoCustosAWS.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class CalculandoCustosAWS {
+    public static void main(String[] args) {
+        // Definindo as taxas
+        double custoPorHoraServidor = 0.15;
+        double custoPorHoraBancoDados = 0.05;
+
+        // Criando um objeto Scanner para receber as entradas do usuário
+        Scanner scanner = new Scanner(System.in);
+
+        // Recebendo as informações do usuário
+        int quantidadeServidores = scanner.nextInt();
+
+        int quantidadeBancosDados = scanner.nextInt();
+
+        //TODO: Calcule os custos
+           double custoTotal;
+           double custoBanco;
+           double custoServidor;
+            custoBanco = quantidadeBancosDados * custoPorHoraBancoDados;
+            custoServidor = quantidadeServidores * custoPorHoraServidor;
+            custoTotal = custoBanco + custoServidor;
+
+
+        System.out.printf("Custo total de uso da AWS por hora: R$ %.2f%n", custoTotal);
+
+        // Fechando o Scanner
+        scanner.close();
+    }
+}
